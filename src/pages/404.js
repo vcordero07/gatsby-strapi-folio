552d2673fd11a70a7ee2c015a8199c9016e5e8a1
vcordero07 <vcordero07@gmail.com>
2022-05-22 18:59:37 -0400

    404 page & footer set

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import Seo from "../components/Seo"
 
 const Error = () => {
   return (
     <>
-      <h2>error page</h2>
+      <main className="error-page">
+        <div className="error-container">
+          <StaticImage
+            src="../assets/images/404.png"
+            alt="404 image"
+            placeholder="blurred"
+          />
+
+          <h3>oops</h3>
+          <Link to="/" className="btn">
+            back home
+          </Link>
+        </div>
+      </main>
     </>
   )
 }
