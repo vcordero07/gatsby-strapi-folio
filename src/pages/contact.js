b901f5cfa70dd675ca2d6b81d4f528a50a540692
vcordero07 <vcordero07@gmail.com>
2022-05-23 21:18:36 -0400

    contact page set

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,36 @@ import Seo from "../components/Seo"
 const contact = () => {
   return (
     <>
-      <h2>contact page</h2>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                cols="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+              <button type="submit" className="submit-btn btn">
+                submit here
+              </button>
+            </div>
+          </form>
+        </article>
+      </section>
     </>
   )
 }
